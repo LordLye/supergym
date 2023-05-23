64ea057d3a3fca832982a80fe4cfc41e1ef4055e
Lye <Lye3@mail.ru>
2023-05-23 21:31:43 +0300

    подключил фавиконку, preload шрифтов, поправил focus, изображения,  мелкие правки

---
+++ source/js/modules/trainer-card.js
@@ -0,0 +1,10 @@
+let trainerCard = document.querySelectorAll('.trainers-card');
+
+function active() {
+  if (document.documentElement.clientWidth < 1200) {
+    // eslint-disable-next-line no-invalid-this
+    this.classList.toggle('trainers-card--active');
+  }
+}
+
+trainerCard.forEach((el) => el.addEventListener('click', active));
