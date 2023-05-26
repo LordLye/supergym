6a7ea0e059e19acdd94f09039f5cdd5fb7d2ce5b
Lye <Lye3@mail.ru>
2023-05-26 09:58:36 +0300

    поправил ошибки линтера в js

--- source/js/modules/trainer-card.js
+++ source/js/modules/trainer-card.js
@@ -1,12 +1,7 @@
-let trainerCard = document.querySelectorAll('.trainers-card');
+let trainerCards = document.querySelectorAll('.trainers-card');
 
-function active() {
-  if (document.documentElement.clientWidth < 1200) {
-    // eslint-disable-next-line no-invalid-this
-    this.classList.toggle('trainers-card--active');
-  }
-}
-
-if (trainerCard) {
-  trainerCard.forEach((el) => el.addEventListener('click', active.call()));
+for (let i = 0; i < trainerCards.length; i++) {
+  trainerCards[i].addEventListener('click', function () {
+    trainerCards[i].classList.toggle('trainers-card--active');
+  });
 }
